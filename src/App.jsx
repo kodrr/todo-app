e96bc6e2958258e6a88c19742a8d2e9612e5b970
brew <brew@gmail.com>
2023-06-25 07:37:41 +0800

    Add form handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,63 @@
+import { useState } from 'react'
 import './style.css'
 
 export default function App() {
+	const [newItem, setNewItem ] = useState('')
+	const [todos, setTodos] = useState([])
+
+function handleFormSubmit(e) {
+	e.preventDefault()
+	console.log(e)
+
+	setTodos(currentTodos=>{
+		return [...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false }]
+	})
+
+	setNewItem('')
+}
+
+function toggleTodo(id, completed){
+	setTodos(currentTodos=>{
+		return currentTodos.map(todo=>{
+			if(todo.id === id){
+				return {...todo, completed}
+			}
+			return todo
+		})
+	})
+}
+
+function deleteTodo(id){
+	setTodos(currentTodos=>{
+		return currentTodos.filter(todo=> todo.id !== id)
+	})
+}
+
  return (
 	<>
-		<form className='new-item-form'>
+		<form onSubmit={handleFormSubmit} className='new-item-form'>
 			<div className='form-row'>
 				<label htmlFor='item'>New Item</label>
-				<input type='text' id='item' />
+				<input value={newItem} onChange={(e)=>setNewItem(e.target.value) } type='text' id='item' />
             </div>
-      		<button className='btn'>Add</button>
-	  		<h1 className='header'>Todo List</h1>
-	  		<ul className='list'>
-				<li>
-					<label>
-						<input type='checkbox' />
-						Item 1
-					</label>
-					<button className='btn btn-danger'>Delete</button>
-				</li>
-				<li>
-					<label>
-						<input type='checkbox' />
-						Item 2
-					</label>
-					<button className='btn btn-danger'>Delete</button>
-				</li>
+			<button className='btn'>Add</button>
+			<h1 className='header'>Todo List</h1>
+			<ul className='list'>
+				{todos.length === 0 && 'No todos'}
+				{todos.map((todo)=>{
+					return (
+						<li key={todo.id}>
+							<label>
+								<input type='checkbox' checked={todo.completed} 
+								onChange={e=>{toggleTodo(todo.id, e.target.checked)}} />
+								{todo.title}
+							</label>
+							<button onClick={()=>deleteTodo(todo.id)} 
+								className='btn btn-danger' >Delete</button>
+						</li>
+					)
+				})}
+
 			</ul>
 		</form>
   </>
